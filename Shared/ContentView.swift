816e1c597b1c8701609b886b74f2b0f4dc3c3f58
Sushant Ubale <subale@apple.com>
2020-08-12 12:16:50 -0500

    First commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView-New.swift
+//  Shared
+//
+//  Created by Sushant-Old on 8/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var searchText : String = ""
+
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: true, content: {
+
+                VStack(spacing: 20) {
+                    
+                    VStack {
+                        SearchBar(text: $searchText)
+                    }
+                    
+                    HStack(spacing: 60) {
+                        ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.blue)
+                            .frame(width: 150, height: 100)
+                        
+                        ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.red)
+                            .frame(width: 150, height: 100)
+                    }
+                    HStack(spacing: 60) {
+                        ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.gray)
+                            .frame(width: 150, height: 100)
+                        
+                        ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.orange)
+                            .frame(width: 150, height: 100)
+                    }
+                    Spacer()
+                }
+            })
+            .navigationTitle("Tasks")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        print("Edit button pressed...")
+                                    }) {
+                                        Image(systemName: "gear")
+                                    })
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
