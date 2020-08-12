419d239f17d73b8120fe14673f64b34645b2501e
Sushant Ubale <subale@apple.com>
2020-08-12 15:51:54 -0500

    Covered the tab bar and table view at the edge

--- Shared/iOS/Views/HomeScreen/TasksView.swift
+++ Shared/iOS/Views/HomeScreen/TasksView.swift
@@ -11,59 +11,65 @@ struct TasksView: View {
     @State private var searchText : String = ""
     
     var body: some View {
-        NavigationView {
-            ScrollView(.vertical, showsIndicators: true, content: {
-                
-                VStack(spacing: 20) {
+        GeometryReader { geo in
+            NavigationView {
+                ScrollView(.vertical, showsIndicators: true, content: {
                     
-                    VStack {
-                        SearchBar(text: $searchText)
-                    }
-                    
-                    HStack(spacing: 60) {
-                        NavigationLink(destination: Text("Details View")) {
-                            ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.green)
-                                .frame(width: 150, height: 100)
+                    VStack(spacing: 20) {
+                        
+                        VStack {
+                            SearchBar(text: $searchText)
                         }
                         
-                        NavigationLink(destination: Text("Details View")) {
-                            ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.red)
-                                .frame(width: 150, height: 100)
+                        HStack(spacing: 60) {
+                            NavigationLink(destination: Text("Details View")) {
+                                ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.green)
+                                    .frame(width: 150, height: 100)
+                            }
+                            
+                            NavigationLink(destination: Text("Details View")) {
+                                ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.red)
+                                    .frame(width: 150, height: 100)
+                            }
+                            
                         }
-                        
+                        HStack(spacing: 60) {
+                            NavigationLink(destination: Text("Details View")) {
+                                ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.blue)
+                                    .frame(width: 150, height: 100)
+                            }
+                            
+                            NavigationLink(destination: Text("Details View")) {
+                                ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.orange)
+                                    .frame(width: 150, height: 100)
+                            }
+                        }
+                        Spacer()
                     }
-                    HStack(spacing: 60) {
-                        NavigationLink(destination: Text("Details View")) {
-                            ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.blue)
-                                .frame(width: 150, height: 100)
-                        }
-                        
-                        NavigationLink(destination: Text("Details View")) {
-                            ButtonView(title: "Inbox", imageName: "tray.and.arrow.down", backgroundColor: Color.orange)
-                                .frame(width: 150, height: 100)
-                        }
-                    }
-                    Spacer()
-                }
-                ListViewWithHeader(listData: [
-                    ListData(headerTitle: "SENDERS", rows: ["Threat Response","Training","Vulnerability Management"]),
-                    ListData(headerTitle: "STATE", rows: ["Pending Acceptance","Assigned","Copied"])
-                ])
-                .frame(height: 300)
-            })
-            .navigationTitle("Tasks")
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                                        print("Edit button pressed...")
-                                    }) {
-                                        Image(systemName: "gear")
-                                    })
+//                    .frame(width: geo.size.width, height: geo.size.height/2, alignment: .center)
+
+                    ListViewWithHeader(listData: [
+                        ListData(headerTitle: "SENDERS", rows: ["Threat Response","Training","Vulnerability Management"]),
+                        ListData(headerTitle: "STATE", rows: ["Pending Acceptance","Assigned","Copied", "Pending Acceptance","Assigned","Copied"])
+                    ])
+                    .frame(width: geo.size.width, height: geo.size.height/2, alignment: .center)
+                })
+                .navigationTitle("Tasks")
+                .navigationBarItems(trailing:
+                                        Button(action: {
+                                            print("Edit button pressed...")
+                                        }) {
+                                            Image(systemName: "gear")
+                                        })
+            }
+            
+
         }
         .tabItem {
             Image(systemName: "checkmark.shield")
             Text("Tasks")
         }.tag(2)
-        
+
     }
 }
 
