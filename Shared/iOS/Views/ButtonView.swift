7ed04919029610c634b5e68c04704ffc6fb357a3
Sushant Ubale <subale@apple.com>
2020-08-23 23:39:56 -0500

    making more generic views for iphone and ipad

--- Shared/iOS/Views/ButtonView.swift
+++ Shared/iOS/Views/ButtonView.swift
@@ -19,66 +19,39 @@ struct ButtonView: View {
     }
     
     var body: some View {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            VStack {
-                HStack(spacing: 100) {
-                    VStack(alignment: .leading, spacing: 40) {
-                        HStack(spacing: 80) {
-                            Image(systemName: imageName)
-                                .renderingMode(.original)
-                                .padding(5)
-                                .background(backgroundColor)
-                                .clipShape(Circle())
-                                .padding(.top, 6)
-                            Text("2")
-                                .font(.title)
+        
+        GeometryReader { geometryReader in
+                VStack {
+                    HStack(spacing: geometryReader.size.width/2) {
+                        VStack(alignment: .leading, spacing: geometryReader.size.width/4) {
+                            HStack(spacing: geometryReader.size.width/2) {
+                                Image(systemName: imageName)
+                                    .renderingMode(.original)
+                                    .padding(5)
+                                    .background(backgroundColor)
+                                    .clipShape(Circle())
+                                    .padding(.top, 6)
+                                Text("4")
+                                    .font(.title)
+                                    .foregroundColor(Color.black)
+                            }
+                            Text(title)
+                                .padding([.leading, .bottom], 5)
                                 .foregroundColor(Color.black)
                         }
-                        Text(title)
-                            .padding([.leading, .bottom], 5)
-                            .foregroundColor(Color.black)
                     }
                 }
-            }
-            .frame(width: (UIScreen.main.bounds.width/3), height: (UIScreen.main.bounds.width/4))
-            .font(.system(size: 20, weight: .semibold, design: .rounded))
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 16, style: .circular))
-            .shadow(color: Color(.blue), radius: 2, x: 2, y: 2)
-            .shadow(color: Color.blue, radius: 2, x: 2, y: 2)
-            .buttonStyle(BorderlessButtonStyle())
-            
-            
-        } else if UIDevice.current.userInterfaceIdiom == .pad {
-            VStack {
-                HStack(spacing: 100) {
-                    VStack(alignment: .leading, spacing: 40) {
-                        HStack(spacing: 80) {
-                            Image(systemName: imageName)
-                                .renderingMode(.original)
-                                .padding(5)
-                                .background(backgroundColor)
-                                .clipShape(Circle())
-                                .padding(.top, 6)
-                            Text("2")
-                                .font(.title)
-                                .foregroundColor(Color.black)
-                        }
-                        Text(title)
-                            .padding([.leading, .bottom], 5)
-                            .foregroundColor(Color.black)
-                    }
-                }
-            }
-            .frame(width: (UIScreen.main.bounds.width/4), height: (UIScreen.main.bounds.width/6))
-            .font(.system(.title))
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 16, style: .circular))
-            .shadow(color: Color(.blue), radius: 2, x: 2, y: 2)
-            .shadow(color: Color.blue, radius: 2, x: 2, y: 2)
-            .buttonStyle(BorderlessButtonStyle())
-            
+                .frame(width: (UIScreen.main.bounds.width/3), height: (UIScreen.main.bounds.width/4))
+                .font(.system(.title))
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .circular))
+                .shadow(color: Color(.blue), radius: 2, x: 2, y: 2)
+                .shadow(color: Color.blue, radius: 2, x: 2, y: 2)
+                .buttonStyle(BorderlessButtonStyle())
+                
+                
         }
+
     }
 }
 
