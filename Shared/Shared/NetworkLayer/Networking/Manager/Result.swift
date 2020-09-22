7616f4a51652c2b9f4e291db947bbe543103a55a
Sushant Ubale <subale@apple.com>
2020-09-22 14:35:53 -0500

    Added generic network layer

---
+++ Shared/Shared/NetworkLayer/Networking/Manager/Result.swift
@@ -0,0 +1,13 @@
+//
+//  Result.swift
+//  ToDoList
+//
+//  Created by Sushant-Old on 9/22/20.
+//
+
+import Foundation
+
+enum Result<String>{
+    case success
+    case failure(String)
+}
--- Shared/Shared/ViewModel/TasksViewModel.swift
+++ Shared/Shared/ViewModel/TasksViewModel.swift
@@ -10,7 +10,7 @@ import Combine
 
 class TasksViewModel: ObservableObject {
     
-    private var tasksNetworkService: TasksNetworkService = TasksNetworkService(dict: ["name": "testName"])
+    private var tasksNetworkService: NetworkManager = NetworkManager()
     var taskData: [String: String] = [:]
     @Published var showActivityIndicator: Bool = true
     @Published var stateViewModel: StateViewModel?
@@ -19,34 +19,9 @@ class TasksViewModel: ObservableObject {
     
     func getTasksData() {
         print("tasksNetworkService.fetchTasksData")
-
         
-        tasksNetworkService.fetchTasksData("http://localhost:9002/graphql?query={todoByPerson{name state}}") { [weak self] (err, result) in
-            guard let strongSelf = self else {return}
-            if(err != nil) {
-                print(err?.localizedDescription as Any)
-                return
-            }
-            if let data = result {
-                let json = try! JSONDecoder().decode(StateViewModel.self, from: data)
-                DispatchQueue.main.async {
-                strongSelf.stateViewModel = json
-                    let mappedData = strongSelf.stateViewModel.map { $0.data.todoByPerson }
-                    if let mappedData = mappedData {
-                        for i in mappedData.enumerated() {
-                            strongSelf.taskData[i.element.name] = i.element.state
-                            if var _ = self?.dummyTaskData[i.element.state] {
-                                self?.dummyTaskData[i.element.state]! += 1
-                            } else {
-                                strongSelf.dummyTaskData[i.element.state] = strongSelf.count + 1
-                            }
-                        }
-                        strongSelf.showActivityIndicator = false
-
-                    }
-                }
-            }
+        tasksNetworkService.getTasks { (stateViewModel, error) in
+            
         }
-
     }
 }
