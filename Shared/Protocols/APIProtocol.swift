ca5832972a0199af1b251ec351e6deb4eab90498
Sushant Ubale <subale@apple.com>
2020-10-14 16:12:35 -0500

    Added test 2 API request

--- Shared/Protocols/APIProtocol.swift
+++ Shared/Protocols/APIProtocol.swift
@@ -32,4 +32,42 @@ extension APIProtocol {
             .eraseToAnyPublisher()
     }
     
+    func multipleRequests<T, S>(_ request: URLRequest,_ decodingType1: T.Type,_ decodingType2: S.Type,_ queue: DispatchQueue = .main,_ retries: Int) -> AnyPublisher<(T, S), Error> where T: Decodable , S: Decodable {
+                
+        guard
+            let postsURL = URL(string: UrlType.posts.rawValue),
+            let commentsURL = URL(string: UrlType.comments.rawValue)
+            else { fatalError("Invalid URL's")}
+        
+        let urlRequest1 = URLRequest(url: commentsURL)
+        let urlRequest2 = URLRequest(url: postsURL)
+
+        let postsPublisher =
+            URLSession.shared.dataTaskPublisher(for: urlRequest1)
+                .map { $0.data }
+                .decode(type: T.self, decoder: JSONDecoder())
+        let commentsPublisher =
+            URLSession.shared.dataTaskPublisher(for: urlRequest2)
+                .map { $0.data }
+                .decode(type: S.self, decoder: JSONDecoder())
+        return
+            Publishers.Zip(postsPublisher, commentsPublisher)
+                .eraseToAnyPublisher()
+    }
+
+}
+
+private enum UrlType: String {
+    case posts = "https://jsonplaceholder.typicode.com/posts"
+    case comments = "https://jsonplaceholder.typicode.com/comments"
+}
+
+struct Post: Codable, Identifiable {
+    let id: Int
+    let title, body: String
+}
+
+struct Comment: Codable, Identifiable {
+    let id, postId: Int
+    let name, email, body: String
 }
